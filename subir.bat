4586671bd4c2cfdaf6fabd599e74056cc76ab2d5
citlali <citlalisalash@gmail.com>
2024-10-01 19:19:00 -0600

    actualizacion

---
+++ subir.bat
@@ -0,0 +1,10 @@
+color a
+cls
+git init
+git add .
+git commit -m "actualizacion"
+git branch -M main
+git remote add origin https://github.com/Citlalisalas/Citlalisalas.github.io.git
+git push -u origin main
+pause
+exist
